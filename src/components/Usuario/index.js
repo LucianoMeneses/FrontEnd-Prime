259717b1bfd6f91f1d3fb6f3a525821a58216a66
LUCIANO ARAUJO MENESES - GETIC - GETIC <214083@int.cagece.com.br>
2022-04-18 23:08:39 -0300

    Correção na PageUsuario

--- src/components/Usuario/index.js
+++ src/components/Usuario/index.js
@@ -3,7 +3,7 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import '../../App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import DataTableCliente from '../Cliente';
 
 function DataTableUsuario() {
